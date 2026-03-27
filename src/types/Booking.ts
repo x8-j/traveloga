17eb869570e90d38c930fa51c87f394fa9c7eaaf
Johann Mendoza <johannmendoza1209@gmail.com>
2026-03-28 00:45:12 +0800

    fix: forgot to add booking ts file

---
+++ src/types/Booking.ts
@@ -0,0 +1,12 @@
+export interface Booking {
+  travellingTo: string;
+  travellingFromLocation: string;
+  regionsCategory: string;
+  flightType: string;
+  dateOfLeave: string;
+  dateOfReturn: string;
+  withHotel: boolean;
+  _id: string;
+  status: string;
+  amount: number;
+}
